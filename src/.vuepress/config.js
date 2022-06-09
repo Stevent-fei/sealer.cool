764ae2724d5460455d099349bfc85dbf1a60489b
wangfei <wf01031309@alibaba-inc.com>
2022-06-09 17:40:50 +0800

    add plugin

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -211,13 +211,15 @@ module.exports = {
     '@vuepress/plugin-medium-zoom',
   ],
 
-  head: [ ['script', {}, `
-        var _hmt = _hmt || []; 
-        (function() { 
-        var hm = document.createElement("script"); 
-        hm.src = "https://www.googletagmanager.com/gtag/js?id=G-5RZSTDS7KT"; 
-        var s = document.getElementsByTagName("script")[0]; 
-        s.parentNode.insertBefore(hm, s); 
-        })(); 
-    `]]
+  head: [
+        ['script', {src: "https://www.googletagmanager.com/gtag/js?id=G-5RZSTDS7KT"},
+        [ `
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
+         
+         gtag('config', 'G-5RZSTDS7KT');
+        `]
+]
+      ]
 }
